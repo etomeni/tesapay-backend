27391c01e53eef4c943178d95c4f6a2293264d2f
Sunday Etom Eni <sundaywht@gmail.com>
2024-10-13 17:41:16 +0100

    rateLimit

--- src/index.ts
+++ src/index.ts
@@ -23,9 +23,11 @@ const limiter = rateLimit({
 	windowMs: 10 * 60 * 1000, // 15 minutes
 	limit: 150, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
 	// standardHeaders: 'draft-7', // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
 	// store: ... , // Redis, Memcached, etc. See below.
-    message: "Too many requests from this IP, please try again later"
+    validate: false,
+    message: "Too many requests from this IP, please try again later",
+
 })
 
 
